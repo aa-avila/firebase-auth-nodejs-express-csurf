7b3a8336f3dc4154a24947f60ceaf8cb50523f03
Agustin <agualvila@gmail.com>
2021-08-18 11:03:18 -0300

    refact profileController & personalDataService

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,10 +1,5 @@
 const PersonalDataService = require('../services/personalDataService');
 
-/** Funcion capitalizar string */
-function PrimeraLetraMayuscula(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 module.exports = class ProfileCtrl {
     // Profile Page
     static async profile_page(req, res, next) {
@@ -15,15 +10,7 @@ module.exports = class ProfileCtrl {
             const userEmail = req.userEmail;
 
             // Get personal data
-            let personalData = await PersonalDataService.getPersonalData(userId);
-
-            // Si no existe, crearla (por ej en caso de que el usuario sea nuevo y no exista aun en la BD)
-            if (personalData === undefined) {
-                const response = await PersonalDataService.createPersonalData(userId);
-                // console.log('response:', response);
-                personalData = await PersonalDataService.getPersonalData(userId);
-                // console.log('new personalData:', personalData);
-            }
+            const personalData = await PersonalDataService.getPersonalData(userId);
 
             // Pack user data
             const userData = {
@@ -34,6 +21,7 @@ module.exports = class ProfileCtrl {
                 phone: personalData.phone
             }
 
+            /** Pasa el csrfToken a la pagina (formulario edit) */
             const csrfToken = req.csrfToken();
 
             res.render('profile', { title: 'Perfil', script: 'profile.js', logged, userData, csrfToken });
@@ -53,36 +41,14 @@ module.exports = class ProfileCtrl {
             const body = req.body;
 
             // Pack personalData
-            let newData = {
+            let formData = {
                 name: body.editProfile_name,
                 lastname: body.editProfile_lastname,
                 phone: body.editProfile_phone,
             }
 
-            /** Si no hay datos en los campos enviados desde el form, obtener datos existentes en BD */
-            if (newData.name === '' || newData.lastname === '' || newData.phone === '') {
-                const currentData = await PersonalDataService.getPersonalData(userId);
-
-                if (newData.name === '') {
-                    newData.name = currentData.name;
-                }
-                if (newData.lastname === '') {
-                    newData.lastname = currentData.lastname;
-                }
-                if (newData.phone === '') {
-                    newData.phone = currentData.phone;
-                }
-            }
-
-            /** Mayuscula la primera letra y minuscula el resto */
-            const nameCapitalized = PrimeraLetraMayuscula(newData.name);
-            newData.name = nameCapitalized;
-            const lastnameCapitalized = PrimeraLetraMayuscula(newData.lastname);
-            newData.lastname = lastnameCapitalized;
-            console.log(newData);
-
             /** Actualizar BD */
-            await PersonalDataService.updatePersonalData(userId, newData);
+            await PersonalDataService.updatePersonalData(userId, formData);
 
             res.redirect('/profile');
         } catch (e) {
--- src/services/personalDataService.js
+++ src/services/personalDataService.js
@@ -15,9 +15,18 @@ module.exports = class PersonalDataService {
     // Leer personalData
     static async getPersonalData(uid) {
         try {
-            const response = await PersonalDataModel.getPersonalData(uid);
-            // console.log(response);
-            return response;
+            // Get personal data
+            let personalData = await PersonalDataModel.getPersonalData(uid);
+
+            // Si no existe, crearla (por ej en caso de que el usuario sea nuevo y no exista aun en la BD)
+            if (personalData === undefined) {
+                const response = await PersonalDataModel.createPersonalData(uid);
+                // console.log('response:', response);
+                personalData = await PersonalDataModel.getPersonalData(uid);
+                // console.log('new personalData:', personalData);
+            }
+
+            return personalData;
         } catch (error) {
             console.log(error.message);
             return error.message;
@@ -27,6 +36,27 @@ module.exports = class PersonalDataService {
     // Actualizar personalData
     static async updatePersonalData(uid, data) {
         try {
+            /** Si no hay datos en los campos enviados desde el form, obtener datos existentes en BD */
+            if (data.name === '' || data.lastname === '' || data.phone === '') {
+                const currentData = await PersonalDataModel.getPersonalData(uid);
+
+                if (data.name === '') {
+                    data.name = currentData.name;
+                }
+                if (data.lastname === '') {
+                    data.lastname = currentData.lastname;
+                }
+                if (data.phone === '') {
+                    data.phone = currentData.phone;
+                }
+            }
+
+            /** Todas letras mayusculas para guardar en BD */
+            const nameMayus = data.name.toUpperCase();
+            const lastnameMayus = data.lastname.toUpperCase();
+            data.name = nameMayus;
+            data.lastname = lastnameMayus;
+
             const response = await PersonalDataModel.updatePersonalData(uid, data);
             return response;
         } catch (error) {
